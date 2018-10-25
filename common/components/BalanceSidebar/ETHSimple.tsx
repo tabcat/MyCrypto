994a8ad0a4fdb47df296e71188d691762cd49d2d
Ben Calder <263295+bencalder@users.noreply.github.com>
2018-10-25 01:22:34 -0700

    Add description strings to English translation file. Import wallet and markdown types. Add description to state and wallet to props. Build function to construct description taking input from addr in wallet prop and domain field. Modify validation to account for subdomain.

--- common/components/BalanceSidebar/ETHSimple.tsx
+++ common/components/BalanceSidebar/ETHSimple.tsx
@@ -1,42 +1,79 @@
 import React from 'react';
 import { connect } from 'react-redux';
-
 import { AppState } from '../../features/reducers';
-import { isValidENSName } from '../../libs/validators';
+import { isValidENSAddress } from '../../libs/validators';
 import { ensActions } from 'features/ens';
 import { Input } from 'components/ui';
 import './ETHSimple.scss';
-import translate from '../../translations';
+import { translate, translateRaw } from '../../translations';
+import { walletSelectors } from 'features/wallet';
+import Markdown from 'react-markdown';
+import NewTabLink from 'components/ui/NewTabLink';
 
 interface State {
   domainToCheck: string;
   isValidDomain: boolean;
   isFocused: boolean;
+  isLoading: boolean;
+  description: React.ReactElement<any>;
 }
 
 interface Props {
   domainRequests: AppState['ens']['domainRequests'];
   resolveDomainRequested: ensActions.TResolveDomainRequested;
+  wallet: AppState['wallet']['inst'];
 }
 
-class ETHSimpleClass extends React.PureComponent<Props, State> {
+class ETHSimpleClass extends React.Component<Props, State> {
   public state = {
     isFocused: false,
     isValidDomain: false,
-    domainToCheck: ''
+    isLoading: false,
+    domainToCheck: '',
+    description: this.buildDesc()
   };
+
+  private buildDesc() {
+    let desc: string;
+    let addr: string;
+
+    if (this.props.wallet) addr = this.props.wallet.getAddressString();
+    else addr = translateRaw('ETHSIMPLE_DESC_DEFAULT_NO_ADDR');
+
+    desc =
+      translateRaw('ETHSIMPLE_DESC_0') +
+      translateRaw('ETHSIMPLE_DESC_DEFAULT_SUBDOMAIN') +
+      translateRaw('ETHSIMPLE_DESC_1') +
+      '`' +
+      addr.substring(0, 25) +
+      '...`';
+
+    return (
+      <Markdown
+        escapeHtml={true}
+        unwrapDisallowed={true}
+        allowedTypes={['link', 'emphasis', 'strong', 'code', 'root', 'inlineCode']}
+        renderers={{
+          root: React.Fragment,
+          link: NewTabLink
+        }}
+        source={desc}
+      />
+    );
+  }
 
   public render() {
     const { domainRequests } = this.props;
-
-    // const { activePromo } = this.state;
+    const { isValidDomain, domainToCheck, description } = this.state;
+    const req = domainRequests[domainToCheck];
+    const isLoading = req && !req.data && !req.error;
 
     return (
-      <div className="ENSPortfolio">
+      <div className="ETHSimple">
         {/*<h5 className="ENSPortfolio-title">My Domains</h5>*/}
-        <h5 className="ENSPortfolio-title">{translate('ETHSIMPLE_TITLE')}</h5>
+        <h5 className="ETHSimple-title">{translate('ETHSIMPLE_TITLE')}</h5>
 
-        <div className="ENSPortfolio-description">{translate('ETHSIMPLE_DESCRIPTION')}</div>
+        <div className="ETHSimple-description">{description}</div>
 
         {/*<div className="row form-group">*/}
         {/*<div className="col-xs-12 col-md-6">*/}
@@ -58,30 +95,30 @@ class ETHSimpleClass extends React.PureComponent<Props, State> {
         {/*</div>*/}
         {/*</div>*/}
 
-        <form className="ENSInput" onSubmit={this.onSubmit}>
+        <form className="ETHSimpleInput" onSubmit={this.onSubmit}>
           <div className="input-group-wrapper">
-            <label className="input-group input-group-inline ENSInput-name">
+            <label className="input-group input-group-inline ETHSimpleInput-name">
               <Input
-                // value={domainToCheck}
-                // isValid={!!domainToCheck && isValidDomain}
+                value={domainToCheck}
+                isValid={!!domainToCheck && isValidDomain}
                 className="border-rad-right-0"
                 type="text"
                 placeholder="mydomain"
                 onChange={this.onChange}
                 onFocus={this.onFocus}
                 onBlur={this.onBlur}
-                // disabled={isLoading}
+                disabled={isLoading}
               />
               <span className="input-group-addon">.ethsimple.eth</span>
             </label>
-            {/*{domainToCheck &&*/}
-            {/*!isValidDomain && (*/}
-            {/*<p className="help-block is-invalid">{translate('ENS_INVALID_INPUT')}</p>*/}
-            {/*)}*/}
+            {domainToCheck &&
+              !isValidDomain && (
+                <p className="help-block is-invalid">{translate('ENS_SUBDOMAIN_INVALID_INPUT')}</p>
+              )}
           </div>
           <button
-            className="ENSInput-button btn btn-primary btn-block"
-            // disabled={!isValidDomain || isLoading}
+            className="ETHSimple-button btn btn-primary btn-block"
+            disabled={!isValidDomain || isLoading}
           >
             {translate('ETHSIMPLE_ACTION')}
           </button>
@@ -92,7 +129,7 @@ class ETHSimpleClass extends React.PureComponent<Props, State> {
               href="https://ethsimple.com"
               target="_blank"
               rel="noopener noreferrer"
-              className="ENSPortfolio-logo"
+              className="ETHSimple-logo"
             />
           </div>
         </div>
@@ -103,26 +140,58 @@ class ETHSimpleClass extends React.PureComponent<Props, State> {
   // add delay to namehash computation / getting the availability
   private onChange = (event: React.FormEvent<HTMLInputElement>) => {
     const domainToCheck = event.currentTarget.value.toLowerCase().trim();
-    const isValidDomain = isValidENSName(domainToCheck);
+    const isValidDomain = isValidENSAddress(domainToCheck + '.ethsimple.eth');
+    const description = this.makeDescription(domainToCheck);
     this.setState({
       domainToCheck,
-      isValidDomain
+      isValidDomain,
+      description
     });
   };
 
   private onSubmit = (ev: React.FormEvent<HTMLElement>) => {
     ev.preventDefault();
     const { isValidDomain, domainToCheck } = this.state;
-    return isValidDomain && this.props.resolveDomainRequested(domainToCheck);
+    return isValidDomain && this.props.resolveDomainRequested(domainToCheck + '.ethsimple.eth');
   };
 
   private onFocus = () => this.setState({ isFocused: true });
   private onBlur = () => this.setState({ isFocused: false });
+
+  private makeDescription = (domain: string) => {
+    let desc: string;
+    let addr: string;
+
+    if (this.props.wallet) addr = this.props.wallet.getAddressString();
+    else addr = translateRaw('ETHSIMPLE_DESC_DEFAULT_NO_ADDR');
+
+    desc =
+      translateRaw('ETHSIMPLE_DESC_0') +
+      (domain.length > 0 ? domain : translateRaw('ETHSIMPLE_DESC_DEFAULT_SUBDOMAIN')) +
+      translateRaw('ETHSIMPLE_DESC_1') +
+      '`' +
+      addr.substring(0, 25) +
+      '...`';
+
+    return (
+      <Markdown
+        escapeHtml={true}
+        unwrapDisallowed={true}
+        allowedTypes={['link', 'emphasis', 'strong', 'code', 'root', 'inlineCode']}
+        renderers={{
+          root: React.Fragment,
+          link: NewTabLink
+        }}
+        source={desc}
+      />
+    );
+  };
 }
 
 function mapStateToProps(state: AppState) {
   return {
-    wallet: state.wallet.inst
+    domainRequests: state.ens.domainRequests,
+    wallet: walletSelectors.getWalletInst(state)
   };
 }
 
